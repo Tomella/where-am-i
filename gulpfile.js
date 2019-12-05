192bcfa37025bb9b1bb86b0f905adf5ead1e50cb
Larry Hill <tomella1@gmail.com>
2019-12-05 22:14:01 +1030

    Use latest libraries, refactor for gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -2,31 +2,16 @@
 var gulp = require('gulp');
 
 // Include Our Plugins
-var fs            = require('fs');
-var eslint        = require('gulp-eslint');
-var babel         = require('gulp-babel');
-var concat        = require('gulp-concat');
 var concatCss     = require('gulp-concat-css');
-var rename        = require('gulp-rename');
 
 var directories = {
 	assets:      'web/app',
    source:      'src'
 };
 
-// Lint Task
-gulp.task('lint', function() {
-    return gulp.src('web/app/**/*.js')
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
-});
-
 // Watch Files For Changes
 gulp.task('watch', function() {
-	// We watch both JS and HTML files.
-    gulp.watch('web/app/**/*.js', ['lint']);
-    gulp.watch(directories.source + '/**/*.css', ['concatCss']);
+    gulp.watch(directories.source + '/**/*.css', gulp.series('concatCss'));
 });
 
 gulp.task('concatCss', function () {
@@ -35,7 +20,7 @@ gulp.task('concatCss', function () {
     .pipe(gulp.dest(directories.assets));
 });
 
-gulp.task('build', ['concatCss'])
+gulp.task('build', gulp.series('concatCss'))
 
 // Default Task
-gulp.task('default', ['lint', 'concatCss', 'watch']);
+gulp.task('default', gulp.series('concatCss', 'watch'));
