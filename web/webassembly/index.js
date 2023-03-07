e24ebfdc5bba4e8e43d4e58149af2d777ea36fc4
Larry Hill <Tomella@users.noreply.github.com>
2023-03-07 20:19:44 +1030

    Use async/await instead of promises

--- web/webassembly/index.js
+++ web/webassembly/index.js
@@ -1,17 +1,16 @@
-let squarer, cuber;
-
 async function loadWebAssembly(fileName) {
-   let response = await fetch(fileName)
-      .then(response => response.arrayBuffer())
-      .then(buffer => WebAssembly.compile(buffer))
-      .then(module => { return new WebAssembly.Instance(module) });
+   let request = await fetch(fileName);
+   let response = await request.arrayBuffer();
+   let buffer = await WebAssembly.compile(response);
+   let module = new WebAssembly.Instance(buffer);
+   return module;
 };
 
 
 
-loadWebAssembly('test.wasm')
-   .then(instance => {
-      squarer = instance.exports._Z7squareri;
-      cuber = instance.exports._Z5cuberi;
-      console.log('Finished compiling! Ready when you are...', squarer(2), cuber(2));
-   });
+let instance = await loadWebAssembly('test.wasm');
+
+let squarer = instance.exports._Z7squareri;
+let cuber = instance.exports._Z5cuberi;
+
+console.log('Finished compiling! Ready when you are...', squarer(2), cuber(2));
